d89daf45d3e17fa2c057f06f51a4ee86ccbb824d
shahf14 <48893166+shahf14@users.noreply.github.com>
2019-07-22 20:15:16 +0300

    Update Server.py
    - group all socket function in one class
    - make the code more modular

--- Server.py
+++ Server.py
@@ -4,12 +4,11 @@ import logging
 from ctypes import *
 import xml.etree.ElementTree as ET
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QLabel,QDialogButtonBox, QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout,QGridLayout ,QApplication
+from PyQt5.QtWidgets import QLabel, QDialogButtonBox, QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, \
+    QDialog, QVBoxLayout, QGridLayout, QApplication
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget, QTableWidgetItem, QVBoxLayout
-from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import QThread
-
-""" This class defines a C-like struct """
 
 class Payload(Structure):
     _fields_ = [("id", c_uint32),
@@ -21,127 +20,118 @@ class MyThread(QThread):
     def __init__(self, parent=None):
         super(MyThread, self).__init__(parent=parent)
 
-
     def run(self):
         time.sleep(3)
 
 
 def logger():  # new logging object
+
     global logger
     logging.basicConfig(filename='Server_Record2.log', level=logging.INFO)
     logger = logging.getLogger()
 
-def create_socket():
-    tree = ET.parse('config.xml')
-    root = tree.getroot()
+class Connection():
 
-    try:
-        global logger
-        global host
-        global port
-        global s
-        host = root[0].text  # local network ip
-        port = int(root[1].text)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = ""
+    port = 0
+    opcode = 0
 
+    My_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __init__(self):
+
+        tree = ET.parse('config.xml')
+        root = tree.getroot()
+        # set host and port from config file
+        self.host = root[0].text
+        self.port = int(root[1].text)
         logger.info("New socket has been created")
-        print('Starting up on {} port {}'.format(host,port))
-        return s
-
-    except socket.error as error:  # If something goes wrong with creating new socket
-        logger.error("socket creation error" + error)
+        print('Starting up on {} port {}'.format(self.host, self.port))
 
 
-def bind_socket():
+    def bind_socket(self):
+        try:
+            self.My_socket.bind((self.host, self.port))  # binding host & port
+            logger.info("binding the port : " + str(self.port))
+            print("binding the port : " + str(self.port))
+            self.My_socket.listen(5)  # queue of five client allowed
 
-    try:
-        global host
-        global port
-        s.bind((host, port))  # binding host & port
-        logger.info("binding the port : " + str(port))
-        print("binding the port : " + str(port))
-        s.listen(5)  # queue of five client allowed
-
-    except socket.error as error:  # If something goes wrong with binding
-        logger.error("socket binding error : " + error)
+        except socket.error as error:  # If something goes wrong with binding
+            logger.error("socket binding error : " + error)
 
 
+    def recive(self):
+        global income
+        global table2
 
-def export(opcode):
-    text = "Opcode : " + str(opcode) + " "
-    with open('opcode.bin', 'a+b') as file:
-             file.write(text.encode('ascii'))
+        msg = Payload(0, 0, 0)
 
+        while (True):
 
-def recv():
-    print("start recv")
-    app = QApplication(sys.argv)
-    global income
-    global table2
-    print("open new app")
-    ex = App()
+            print('waiting for a connection..')
+            conn, addr = self.My_socket.accept()
+            print("connection has been established | " + repr(addr))
+            logger.info("connection has been established | " + repr(addr))
 
-    msg = Payload(0, 0, 0)
+            while conn:
+                myThread = MyThread()
 
-    while (True):
+                buff = conn.recv(sizeof(msg))
 
-        print('waiting for a connection..')
-        conn, addr = s.accept()
-        print("connection has been established | " + repr(addr))
-        logger.info("connection has been established | " + repr(addr))
+                print("recv %d bytes" % sizeof(msg))
+                payload_in = Payload.from_buffer_copy(buff)
 
-        while conn:
-            myThread = MyThread()
+                print(f"Received id={payload_in.id}, counter={payload_in.counter}, opcode={payload_in.opcode}")
 
-            buff = conn.recv(sizeof(msg))
+                payload_out = payload_in
 
-            print("recv %d bytes" % sizeof(msg))
-            payload_in = Payload.from_buffer_copy(buff)
+                self.opcode = payload_in.opcode
 
-            print(f"Received id={payload_in.id}, counter={payload_in.counter}, opcode={payload_in.opcode}")
+                if self.opcode == 1:
+                    self.export()
+                elif self.opcode == 2:
+                    payload_out.counter += 1
+                else:
+                    logger.info("Unexpected opcode %d" % self.opcode)
 
-            payload_out = payload_in
+                nsent = conn.send(payload_out)
+                print("send %d bytes" % nsent)
+                print("send id=%d, counter=%d, opcode=%d" % (payload_out.id,
+                                                             payload_out.counter,
+                                                             payload_out.opcode))
+                self.setNewTable(payload_in, payload_out)
 
-            opcode = payload_in.opcode
+        print("Closing connection to client")
+        print("----------------------------")
 
-            if opcode == 1:
-                export(opcode)
-            elif opcode == 2:
-                payload_out.counter += 1
-            else:
-                logger.info("Unexpected opcode %d" % opcode)
+        sys.exit()
 
-            nsent = conn.send(payload_out)
-            print("send %d bytes" % nsent)
-            print("send id=%d, counter=%d, opcode=%d" % (payload_out.id,
-                                                         payload_out.counter,
-                                                         payload_out.opcode))
-            print("start insert")
-            insertValues(table1, payload_in)
-            insertValues(table2, payload_out)
-            print("show")
-
+    def setNewTable(self,payload_in, payload_out):
+            self.insertValues(table1, payload_in)
+            self.insertValues(table2, payload_out)
             ex.show()
-            print("end show")
             app.exec_()
-            print("loop end")
+
+    def insertValues(self,table, payload):
+            table.setItem(0, 1, QTableWidgetItem(str(payload.id)))
+            table.setItem(1, 1, QTableWidgetItem(str(payload.counter)))
+            table.setItem(2, 1, QTableWidgetItem(str(payload.opcode)))
+
+    def export(self):
+        text = "Opcode : " + str(self.opcode) + " "
+        with open('opcode.bin', 'a+b') as file:
+            file.write(text.encode('ascii'))
 
 
-    print("Closing connection to client")
-    print("----------------------------")
-
-    sys.exit()
-
-
-def insertValues(table, payload):
-    table.setItem(0, 1, QTableWidgetItem(str(payload.id)))
-    table.setItem(1, 1, QTableWidgetItem(str(payload.counter)))
-    table.setItem(2, 1, QTableWidgetItem(str(payload.opcode)))
 
 class App(QWidget):
 
     def __init__(self, parent=None):
+
+
+
         super(QWidget, self).__init__(parent=parent)
+
         global table1
         global table2
         QTableWidget.setMinimumSize(self, 1000, 500)
@@ -168,7 +158,7 @@ class App(QWidget):
         self.horizontalLayout.addWidget(self.table1)
         self.horizontalLayout.addWidget(self.table2)
         self.horizontalLayout.setContentsMargins(0, 20, 0, 0)
-        table1= self.table1
+        table1 = self.table1
         table2 = self.table2
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.verticalLayout.addWidget(self.buttonBox)
@@ -190,13 +180,16 @@ class App(QWidget):
         table.horizontalHeader().setStretchLastSection(True)
 
 
-
-
 def main():
+    global app, ex
+    app = QApplication(sys.argv)
+    ex = App()
     logger()
-    create_socket()
-    bind_socket()
-    recv()
+    socket = Connection()
+    socket.bind_socket()
+    socket.recive()
+
+
 
 if __name__ == '__main__':
     main()
