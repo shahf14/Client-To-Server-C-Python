7c6773ab37f5d7b1d6b5bb5b105d99bd782b7a57
shahf14 <48893166+shahf14@users.noreply.github.com>
2019-07-22 14:19:06 +0300

    Update Server.py
    - fix Export() function 
    - add GUI interface
    - bug fixed

--- Server.py
+++ Server.py
@@ -7,12 +7,23 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import QLabel,QDialogButtonBox, QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout,QGridLayout ,QApplication
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget, QTableWidgetItem, QVBoxLayout
 from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
+from PyQt5.QtCore import QThread
 
 """ This class defines a C-like struct """
+
 class Payload(Structure):
     _fields_ = [("id", c_uint32),
                 ("counter", c_uint32),
                 ("opcode", c_uint32)]
+
+
+class MyThread(QThread):
+    def __init__(self, parent=None):
+        super(MyThread, self).__init__(parent=parent)
+
+
+    def run(self):
+        time.sleep(3)
 
 
 def logger():  # new logging object
@@ -56,14 +67,78 @@ def bind_socket():
 
 
 
-# def export(opcode):
-#     with open('opcode.bin', 'a+b') as file:
-#             file.write(opcode.encode())
+def export(opcode):
+    text = "Opcode : " + str(opcode) + " "
+    with open('opcode.bin', 'a+b') as file:
+             file.write(text.encode('ascii'))
 
+
+def recv():
+    print("start recv")
+    app = QApplication(sys.argv)
+    global income
+    global table2
+    print("open new app")
+    ex = App()
+
+    msg = Payload(0, 0, 0)
+
+    while (True):
+
+        print('waiting for a connection..')
+        conn, addr = s.accept()
+        print("connection has been established | " + repr(addr))
+        logger.info("connection has been established | " + repr(addr))
+
+        while conn:
+            myThread = MyThread()
+
+            buff = conn.recv(sizeof(msg))
+
+            print("recv %d bytes" % sizeof(msg))
+            payload_in = Payload.from_buffer_copy(buff)
+
+            print(f"Received id={payload_in.id}, counter={payload_in.counter}, opcode={payload_in.opcode}")
+
+            payload_out = payload_in
+
+            opcode = payload_in.opcode
+
+            if opcode == 1:
+                export(opcode)
+            elif opcode == 2:
+                payload_out.counter += 1
+            else:
+                logger.info("Unexpected opcode %d" % opcode)
+
+            nsent = conn.send(payload_out)
+            print("send %d bytes" % nsent)
+            print("send id=%d, counter=%d, opcode=%d" % (payload_out.id,
+                                                         payload_out.counter,
+                                                         payload_out.opcode))
+            print("start insert")
+            insertValues(table1, payload_in)
+            insertValues(table2, payload_out)
+            print("show")
+
+            ex.show()
+            print("end show")
+            app.exec_()
+            print("loop end")
+
+
+    print("Closing connection to client")
+    print("----------------------------")
+
+    sys.exit()
+
+
+def insertValues(table, payload):
+    table.setItem(0, 1, QTableWidgetItem(str(payload.id)))
+    table.setItem(1, 1, QTableWidgetItem(str(payload.counter)))
+    table.setItem(2, 1, QTableWidgetItem(str(payload.opcode)))
 
 class App(QWidget):
-    global table1
-    global table2
 
     def __init__(self, parent=None):
         super(QWidget, self).__init__(parent=parent)
@@ -73,13 +148,13 @@ class App(QWidget):
         QTableWidget.setWindowTitle(self, "Server Massages")
         self.table1 = QTableWidget()
         self.configureTable(self.table1)
-        table1 = self.table1
+
         label = QLabel('Income', self)
         label.move(235, 0)
 
         self.table2 = QTableWidget()
         self.configureTable(self.table2)
-        table2 = self.table2
+
         label = QLabel('Outcome', self)
         label.move(725, 0)
 
@@ -92,17 +167,16 @@ class App(QWidget):
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.addWidget(self.table1)
         self.horizontalLayout.addWidget(self.table2)
-        self.horizontalLayout.setContentsMargins(0,20,0,0)
-
+        self.horizontalLayout.setContentsMargins(0, 20, 0, 0)
+        table1= self.table1
+        table2 = self.table2
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.verticalLayout.addWidget(self.buttonBox)
 
         self.buttonBox.accepted.connect(self.close)
         self.buttonBox.rejected.connect(self.close)
 
-
     def configureTable(self, table):
-
         rowf = 3
         table.setColumnCount(2)
         table.setRowCount(rowf)
@@ -116,81 +190,14 @@ class App(QWidget):
         table.horizontalHeader().setStretchLastSection(True)
 
 
-    def recv(self):
-
-        msg = Payload(0, 0, 0)
-        while (1):
-            global table1
-            global table2
-
-            print('waiting for a connection..')
-            conn, addr = s.accept()
-            print("connection has been established | " + repr(addr))
-            logger.info("connection has been established | " + repr(addr))
-
-
-            while conn:
-
-                buff = conn.recv(sizeof(msg))
-
-                print("recv %d bytes" % sizeof(msg))
-                payload_in = Payload.from_buffer_copy(buff)
-
-                print("Received id=%d, counter=%d, opcode=%d" % (payload_in.id,
-                                                                 payload_in.counter,
-                                                                 payload_in.opcode))
-
-                payload_out = payload_in
-
-                opcode = payload_in.opcode
-
-                # if opcode == 1 :
-                # export(opcode)
-
-                # el
-                if opcode == 2:
-                    payload_out.counter += 1
-                else:
-                    logger.info("Unexpected opcode %d" % opcode)
-
-
-                nsent = conn.send(payload_out)
-                print("send %d bytes" % nsent)
-                print("send id=%d, counter=%d, opcode=%d" % (payload_out.id,
-                                                             payload_out.counter,
-                                                             payload_out.opcode))
-
-
-
-                self.insertValues(table1, payload_in)
-                self.insertValues(table2, payload_out)
-                self.show()
-
-                # app.exec_()
-
-        # sys.exit(app.exec_())
-        print("Closing connection to client")
-        print("----------------------------")
-        ssock.close()
-
-    def insertValues (self, table, payload):
-
-        table.setItem(0, 1, QTableWidgetItem(str(payload.id)))
-        table.setItem(1, 1, QTableWidgetItem(str(payload.counter)))
-        table.setItem(2, 1, QTableWidgetItem(str(payload.opcode)))
-
-
 
 
 def main():
-    global s
     logger()
     create_socket()
     bind_socket()
-    app = QApplication(sys.argv)
-    ex = App()
-    ex.recv()
-    sys.exit(app.exec_())
+    recv()
 
+if __name__ == '__main__':
+    main()
 
-main()
